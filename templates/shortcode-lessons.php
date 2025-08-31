b810f0725faeec99c0a42d7e0fb12b8716d96acb
AtravaHamid <114897895+AtravaHamid@users.noreply.github.com>
2025-08-31 23:35:53 +0330

    Add lesson CPT, shortcode, and conditional assets

--- templates/shortcode-lessons.php
+++ templates/shortcode-lessons.php
@@ -2,6 +2,8 @@
 $id = $args['id'] ?? get_the_ID();
 ?>
 <article class="ielts-card">
-  <h3><a href="<?php echo esc_url(get_permalink($id)); ?>"><?php echo esc_html(get_the_title($id)); ?></a></h3>
-  <p><?php echo esc_html(get_the_excerpt($id)); ?></p>
+  <h3><a href="<?php echo esc_url( get_permalink( $id ) ); ?>"><?php echo esc_html( get_the_title( $id ) ); ?></a></h3>
+  <?php if ( has_excerpt( $id ) ) : ?>
+    <p><?php echo esc_html( get_the_excerpt( $id ) ); ?></p>
+  <?php endif; ?>
 </article>
